591fe2b3911fa122edd36a5027a99de54cbece6f
Nishat-M <nishat.mohammad19@gmail.comnjfgjfdfd>
2024-10-21 10:47:58 +0600

    home dynamic section title is transferred to category

--- restaurant-front/src/Component/HomePage/Category/Category.jsx
+++ restaurant-front/src/Component/HomePage/Category/Category.jsx
@@ -6,54 +6,75 @@ import dessert from "../../../assets/category-images/category-dessert.png";
 import "./CategoryStyle.css";
 
 import { Link } from "react-router-dom";
+import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
 const Category = () => {
   return (
-    <div className="pb-12 bg-red-700">
-      <div className="marquee-container mb-6 lg:mb-12">
-        <p className="marquee capitalize lg:text-2xl text-gray-200">
-        <span className="text-4xl lg:text-6xl">🛵</span> Free delivery everyday Gulshan : 0150 111 8888 (12pm - 1am), Uttara :
-          0150 111 8888 (12pm - 11pm), Mirpur : 0150 111 8888 (12pm - 11pm),
-          Bashundhara : 0150 111 8888 (12pm - 10pm), Banani : 0170 999 0777
-          (12pm - 12am), Dhanmondi : 0150 111 8888 (12pm - 12am) , Khilgaon :
-          0150 111 8888 (12pm - 10pm)
-        </p>
-      </div>
-
-      <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row flex-wrap gap-12 lg:gap-20 justify-center items-center cinzel-bold">
-        <Link to={"/"} className="">
-          <img className="w-20 md:w-24 lg:w-32 lg:mb-4" src={pizza} alt="soup-photo" />
-          <h6 className="text-center text-lg lg:text-2xl text-gray-200 font-bold">
-            PIZZA
-          </h6>
-        </Link>
-        <Link to={"/"} className="">
-          <img className="w-20 lg:w-32 lg:mb-4" src={burger} alt="soup-photo" />
-          <h6 className="text-center text-lg lg:text-2xl text-gray-200 font-bold">
-            BURGER
-          </h6>
-        </Link>
-        <Link to={"/"} className="">
-          <img className="w-20 lg:w-32 lg:mb-4" src={soup} alt="soup-photo" />
-          <h6 className="text-center text-lg lg:text-2xl text-gray-200 font-bold">
-            SOUP
-          </h6>
-        </Link>
-        <Link to={"/"} className="">
-          <img className="w-20 lg:w-32 lg:mb-4" src={salad} alt="soup-photo" />
-          <h6 className="text-center text-lg lg:text-2xl text-gray-200 font-bold">
-            SALAD
-          </h6>
-        </Link>
-        <Link to={"/"} className="">
-          <img
-            className="w-20 lg:w-32 lg:mb-4"
-            src={dessert}
-            alt="soup-photo"
-          />
-          <h6 className="text-center text-lg lg:text-2xl text-gray-200 font-bold">
-            DESERT
-          </h6>
-        </Link>
+    <div className="pb-12">
+      <section>
+        <SectionTitle
+          heading={"Order Online"}
+          subHeading={"From 12PM to 12AM"}
+        ></SectionTitle>
+      </section>
+      <div className="bg-red-700 pb-12">
+        <div className="marquee-container mb-6 lg:mb-12">
+          <p className="marquee capitalize lg:text-2xl text-gray-200">
+            <span className="text-4xl lg:text-6xl">🛵</span> Free delivery
+            everyday Gulshan : 0150 111 8888 (12pm - 1am), Uttara : 0150 111
+            8888 (12pm - 11pm), Mirpur : 0150 111 8888 (12pm - 11pm),
+            Bashundhara : 0150 111 8888 (12pm - 10pm), Banani : 0170 999 0777
+            (12pm - 12am), Dhanmondi : 0150 111 8888 (12pm - 12am) , Khilgaon :
+            0150 111 8888 (12pm - 10pm)
+          </p>
+        </div>
+        <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row flex-wrap gap-12 lg:gap-20 justify-center items-center cinzel-bold">
+          <Link to={"/"} className="">
+            <img
+              className="w-20 md:w-24 lg:w-32 lg:mb-4"
+              src={pizza}
+              alt="soup-photo"
+            />
+            <h6 className="text-center text-lg lg:text-2xl text-gray-200 font-bold">
+              PIZZA
+            </h6>
+          </Link>
+          <Link to={"/"} className="">
+            <img
+              className="w-20 lg:w-32 lg:mb-4"
+              src={burger}
+              alt="soup-photo"
+            />
+            <h6 className="text-center text-lg lg:text-2xl text-gray-200 font-bold">
+              BURGER
+            </h6>
+          </Link>
+          <Link to={"/"} className="">
+            <img className="w-20 lg:w-32 lg:mb-4" src={soup} alt="soup-photo" />
+            <h6 className="text-center text-lg lg:text-2xl text-gray-200 font-bold">
+              SOUP
+            </h6>
+          </Link>
+          <Link to={"/"} className="">
+            <img
+              className="w-20 lg:w-32 lg:mb-4"
+              src={salad}
+              alt="soup-photo"
+            />
+            <h6 className="text-center text-lg lg:text-2xl text-gray-200 font-bold">
+              SALAD
+            </h6>
+          </Link>
+          <Link to={"/"} className="">
+            <img
+              className="w-20 lg:w-32 lg:mb-4"
+              src={dessert}
+              alt="soup-photo"
+            />
+            <h6 className="text-center text-lg lg:text-2xl text-gray-200 font-bold">
+              DESERT
+            </h6>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- restaurant-front/src/Pages/Home.jsx
+++ restaurant-front/src/Pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Banner from "../Component/Shared/Banner/Banner";
 import Category from "../Component/HomePage/Category/Category";
-import SectionTitle from "../Component/Shared/SectionTitle/SectionTitle";
 import PopularMenu from "../Component/HomePage/PopularMenu/PopularMenu";
 import Testimonials from "../Component/HomePage/Testimonials/Testimonials";
 import SignatureDish from "../Component/HomePage/SignatureDish/SignatureDish";
@@ -10,10 +9,6 @@ const Home = () => {
   return (
     <div>
       <Banner></Banner>
-      <SectionTitle
-        heading={"Order Online"}
-        subHeading={"From 12PM to 12AM"}
-      ></SectionTitle>
       <Category></Category>
       <PopularMenu></PopularMenu>
       <SignatureDish></SignatureDish>
