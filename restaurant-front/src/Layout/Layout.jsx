db11e77811eb614dc74e9ac98ae30941a98635ce
Nishat-M <nishat.mohammad19@gmail.comnjfgjfdfd>
2024-10-28 10:22:42 +0600

    scrollToTop function added to the layout page to controll scroll while changing routes

--- restaurant-front/src/Layout/Layout.jsx
+++ restaurant-front/src/Layout/Layout.jsx
@@ -1,17 +1,27 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../Component/Shared/Navbar";
 import Footer from "../Component/Shared/Footer";
 import "./restaurant-style.css";
 import CustomToast from "../Component/Shared/CustomToast/CustomToast";
 
 const Layout = () => {
+    function ScrollToTop() {
+        const { pathname } = useLocation();
+    
+        useEffect(() => {
+          window.scrollTo(0, 0);
+        }, [pathname]);
+    
+        return null;
+      }
   return (
     <div>
       <CustomToast></CustomToast>
       <div className="flex flex-col min-h-screen lora-regular">
         <Navbar></Navbar>
         <div className="flex-grow">
+            <ScrollToTop></ScrollToTop>
           <Outlet></Outlet>
         </div>
         <Footer></Footer>
